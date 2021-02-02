b4216a4e7edbea0a486ed745fa5102708d7821aa
mjalalit <mj3821@gmail.com>
2021-02-01 23:49:43 -0800

    week2-updated

--- lecture/week_1/HelloWorld.java
+++ lecture/week_1/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.print("Hello world!");
+        System.out.println("Comp110");
     }
+   
 }
