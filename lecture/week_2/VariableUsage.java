8f71ea474278fc8077e07ba54b000a5767ca2803
mjalalit <mj3821@gmail.com>
2021-02-08 23:53:14 -0800

    week3-updated

--- lecture/week_2/VariableUsage.java
+++ lecture/week_2/VariableUsage.java
@@ -1,7 +1,7 @@
 public class VariableUsage {
     public static void main(String[] args) {
         int num = 7;
-        int otherNum = 8;
+            int otherNum = 8;
         int addedTogether = num + otherNum;
 
         // prints out -1
