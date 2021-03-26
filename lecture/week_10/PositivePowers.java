537238b13f0cb36af37a2b779d0d78afb876df4d
mjalalit <mj3821@gmail.com>
2021-03-26 09:29:12 -0700

    addedLab

---
+++ lecture/week_10/PositivePowers.java
@@ -0,0 +1,18 @@
+//Print positive powers of 2 up 20 2^20
+
+
+public class PositivePowers {
+
+//Print positive powers of 2 up 30// 2^30
+
+   public static void main (String [] args){
+   
+       for (int i=0; i <=4;i++){
+         System.out.println(Math.pow(2,i));
+
+       }
+       
+   
+  }
+
+}
