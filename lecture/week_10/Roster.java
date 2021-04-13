aa02a57d9b094f34e3a97871f0ccdbd5872ca5c6
mjalalit <mj3821@gmail.com>
2021-04-13 01:11:18 -0700

    newWeek

---
+++ lecture/week_10/Roster.java
@@ -0,0 +1,23 @@
+public class Roster {
+
+   public static void main(String[] args) {
+
+      
+      Student[] all = new Student[2];
+      
+      all[0] = new Student(10, "John");
+      all[1] = new Student(20, "Josh");
+
+      System.out.println(all[0]);
+
+    }
+}
+
+
+
+
+
+
+
+
+
---
+++ lecture/week_10/Student.java
@@ -0,0 +1,16 @@
+public class Student {
+
+   int studentId;
+   String lastName;
+
+   public Student (int id, String name){
+      studentId = id;
+      lastName = name;
+
+   }
+   public String toString(){
+      return lastName;
+
+   }
+
+}
