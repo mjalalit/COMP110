00156abe1bfc90c7d0abdc2cff3d8525026e6186
mjalalit <mj3821@gmail.com>
2021-05-03 22:32:26 -0700

    newTopic14

---
+++ lecture/week_14/Test.java
@@ -0,0 +1,17 @@
+public class Test {
+
+   public static int DivOp (int a, int b) {
+   
+      return a/b;
+   
+   }
+   
+
+   public static void main(String [] args) {
+      int[] a = new int[] {1,2,3};
+      System.out.println(a[5]);     
+    //  System.out.println(DivOp(8,0));
+      System.out.println("Here");
+
+   }
+}
